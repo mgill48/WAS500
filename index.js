30fdb7d11f7c1df88b16de3a9c9b7d5ec7d01e7c
mgill48 <117995808+mgill48@users.noreply.github.com>
2022-11-11 12:48:40 -0500

    Commit 1

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-console.log("Hello World!!")
+const port = 3000;
+const http = require("http");
+const httpStatus = require("http-status-codes");
+const fs = require("fs");
+
+const getViewURL = (url) => {
+  return `views${url}.html`;
+};
+
+const app = http.createServer();
+app.on("request", (req, res) => {
+  console.log(`Received an incoming request...`);
+
+  let viewUrl = getViewURL(req.url);
+
+  fs.readFile(viewUrl, (error, data) => {
+    if (error) {
+      res.writeHead(httpStatus.StatusCodes.NOT_FOUND, {
+        "Content-Type": "text/html",
+      });
+      res.write("<h1>Error page!</h1>");
+      res.end();
+    } else if (data) {
+      res.writeHead(httpStatus.StatusCodes.OK, {
+        "Content-Type": "text/html",
+      });
+      res.write(data);
+      res.end();
+    }
+  });
+});
+app.listen(port);
+console.log(`The server has started and is listening on port number:${port}`);
